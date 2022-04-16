df3fa8f20af27546dd9b009a76555f68c4ef0c11
Steph <xsabienx@gmail.com>
2022-04-15 23:26:06 -0400

    single page app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 //import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 //import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>   
         );
     }
 }
